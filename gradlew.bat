a9b14fb736297c2437d9485fc97e232762fd3806
Meghna Sahu <me.ghnasahu0015@gmail.com>
2026-09-05 20:44:28 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -64,7 +64,6 @@ goto fail
 @rem Get command-line arguments, handling Windows variants
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
-
 :win9xME_args
 @rem Slurp the command line arguments.
 set CMD_LINE_ARGS=
